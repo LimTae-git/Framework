861f0d6405dcb4b9a0852acb02c3bd06d7aad52c
LimTae-git <lth9122@naver.com>
2019-10-17 15:28:38 +0900

    update

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var path = require('path');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
-var sanitizeHtml = require('sanitize-html');
 var compression = require('compression');
-var template = require('./lib/template.js');
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+// helmet을 이용한 애플리케이션 안전하게 보호
+var helmet = require('helmet')
+app.use(helmet());
 
 // 남이 만든 미들웨어 사용하기
 app.use(express.static('public'));  // 정적인 파일 서비스하기
@@ -22,117 +23,9 @@ app.get('*', function(request, response,next){
 
 // route, routing
 // app.get('/', (req,res) => res.send('Hello World!'))
-app.get('/', function(request, response) {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `
-    <h2>${title}</h2>${description}
-    <img src = "images/hello.jpg" style = "width: 300px; display:block; margin-top: 10px;">
-    `,
-    `<a href="/topic/create">create</a>`
-  );
-  response.send(html);
-});
 
-app.get('/topic/create', function(request, response) {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-    <p><input type="text" name="title" placeholder="title"></p>
-    <p>
-    <textarea name="description" placeholder="description"></textarea>
-    </p>
-    <p>
-    <input type="submit">
-    </p>
-    </form>
-    `, '');
-    response.send(html);
-  });
-
-  app.post('/topic/create_process', function(request, response){
-    var post = request.body;
-    var title = post.title;
-    var description = post. description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/topic/${title}`)
-    })
-  });
-
-  app.get('/topic/update/:pageId', function(request, response){
-    var filterdId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filterdId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(request.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/topic/update_process" method="post">
-        <input type = "hidden" name = "id" value = "${title}">
-        <p><input type="text" name="title" placeholder="title" value = "${title}"></p>
-        <p>
-        <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-        <input type="submit">
-        </p>
-        </form>
-        `,
-        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-      );
-      response.send(html);
-    });
-  });
-
-  app.post('/topic/update_process', function(request, response){
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post. description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect(`/topic/${title}`);
-      })
-    })
-  });
-
-  app.post('/topic/delete_process', function(request, response){
-    var post = request.body;
-    var id = post.id;
-    var filterdId = path.parse(id).base;
-    fs.unlink(`data/${filterdId}`, function(error){
-      response.redirect(`/`);
-    })
-  });
-
-  app.get('/topic/:pageId', function(request, response, next) {
-    var filterdId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filterdId}`, 'utf8', function(err, description){
-      if(err){
-        next(err);
-      } else {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(request.list);
-        var html = template.HTML(title, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action = "/topic/delete_process" method = "post">
-          <input type = "hidden" name = "id" value = "${sanitizedTitle}">
-          <input type = "submit" value = "delete">
-          </form>`
-        );
-        response.send(html);
-      }
-    });
-  });
-
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
 
   // 에러처리
@@ -149,43 +42,3 @@ app.get('/topic/create', function(request, response) {
   app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
   });
-
-  /*
-  var http = require('http');
-  var fs = require('fs');
-  var url = require('url');
-  var qs = require('querystring');
-  var template = require('./lib/template.js');
-  var path = require('path');
-  var sanitizeHtml = require('sanitize-html');
-
-
-  var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  if(pathname === '/'){
-  if(queryData.id === undefined){
-} else {
-}
-} else if(pathname === '/create'){
-});
-} else if(pathname === '/create_process'){
-
-
-} else if(pathname === '/update'){
-
-} else if(pathname === "/update_process"){
-
-} else if(pathname === "/delete_process"){
-
-} else {
-response.writeHead(404);
-response.end('Not found');
-}
-
-
-
-});
-app.listen(3000);
-*/
